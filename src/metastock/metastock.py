6c6c5dec4aa756c2864187ea2d7ec9fb59316efc
Joe Trader <traderjoe1968@gmail.com>
2022-12-09 08:50:54 +1100

    Cleanup

--- src/metastock/metastock.py
+++ src/metastock/metastock.py
@@ -368,29 +368,3 @@ class MSDataFrame:
 
     def getList(self):
         return self.symList
-
-
-if __name__ == "__main__":
-    import src as ms
-
-    rootPath = r"Z:\Data\Futures"
-    d = []
-    classPath = os.path.join(rootPath, "Continuous Contracts")
-    for root, dirs, files in os.walk(classPath):
-        d.extend(dirs)
-    # print(d)
-
-    contractPath = os.path.join(
-        os.path.join(rootPath, "Continuous Contracts"), "Back Adjusted"
-    )
-    msObj = ms.MSDirectory(contractPath)
-    # print(msObj)
-    d = []
-    for item in msObj:
-        d.append(item.name)
-    # print(d)
-    msObj = ms.MSDataFrame(contractPath)
-    symData = msObj.get("ES___CCB")
-    print()
-    #print(f"Name:{symData.name},Symbol:{symData.symbol}")
-    print(symData.tail(5))
